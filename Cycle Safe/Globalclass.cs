50a72dbb91919b29e6559eb91739e9e70d5066c6
Jeremy Cagle <jlc304@txstate.edu>
2016-02-23 23:23:16 -0600

    Added Global Variable class to store user information

---
+++ Cycle Safe/Globalclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycle_Safe
+{
+    class Globalclass
+    {
+        public static string fullname = "";
+        public static string username = "";
+        public static string phone = "";
+        public static string email = "";
+        public static string password = "";
+    }
+}
